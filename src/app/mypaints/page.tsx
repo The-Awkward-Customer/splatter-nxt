7081492ff04e39721c4e73fbda98a9f45d5a862a
PETER ABBOTT <abbotsayshi@gmail.com>
2024-01-25 16:03:11 +0000

    added db query

--- src/app/mypaints/page.tsx
+++ src/app/mypaints/page.tsx
@@ -1,26 +1,39 @@
-import { Interface } from "readline";
+import { db } from "@/lib/db";
 
-// interface type because fetch returns an object
-interface Post {
-  id: number;
-  title: string;
+interface PaintObj {
+  name: string;
+  product_type: string;
+  brand_name: string;
+  company_name: string;
+  colors: string;
 }
 
 export default async function MyPaints() {
   //api call
   //fetch await (function needs to be async)
-  //use response
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts"); // call the api
-  // response will return an array?
-  const posts = await response.json(); // parse the response as json
-  console.log(posts);
+
+  //pg uses sql queries rather than a fetch
+  const res = await db.query(`SELECT 
+  products.name, 
+  categories.product_type, 
+  brand.brand_name,      
+  company.company_name, 
+  STRING_AGG(colors.color, ', ' ORDER BY colors.color) AS colors
+FROM products
+JOIN categories ON products.category_id = categories.id
+JOIN brand ON products.brand_id = brand.id
+JOIN company ON brand.company_id = company.id
+JOIN product_color_junction ON products.id = product_color_junction.product_id
+JOIN colors ON product_color_junction.color_id = colors.id
+GROUP BY products.name, categories.product_type, brand.brand_name, company.company_name`); // call the api
+  console.log(res.rows);
 
   // map through retreived data and populate map
   return (
     <>
       <h2>MyPaints</h2>
-      {posts.map((post: Post) => (
-        <li key={post.id}>{post.title}</li>
+      {res.rows.map((paintObj: PaintObj) => (
+        <li key={paintObj.name}>{paintObj.name}</li>
       ))}
     </>
   );
