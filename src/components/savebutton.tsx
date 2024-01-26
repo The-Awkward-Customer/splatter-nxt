6de0c314cc03184b49d6befe1d0a73aacc4687f5
PETER ABBOTT <abbotsayshi@gmail.com>
2024-01-26 14:45:00 +0000

    attempt 1 removed imports and commented out component

--- src/components/savebutton.tsx
+++ src/components/savebutton.tsx
@@ -1,13 +1,13 @@
-"use client";
-import { useFormStatus } from "react-dom";
+// "use client";
+// import { useFormStatus } from "react-dom";
 
-export default function SaveButton() {
-  const { pending } = useFormStatus();
-  console.log(pending);
+// export default function SaveButton() {
+//   const { pending } = useFormStatus();
+//   console.log(pending);
 
-  return (
-    <button disabled={pending} className={pending ? "disabled" : ""}>
-      {pending ? "uploading" : "submit"}
-    </button>
-  );
-}
+//   return (
+//     <button disabled={pending} className={pending ? "disabled" : ""}>
+//       {pending ? "uploading" : "submit"}
+//     </button>
+//   );
+// }
