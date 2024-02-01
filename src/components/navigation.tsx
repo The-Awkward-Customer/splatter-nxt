7ec24dbe29af1fd8902d28fa0d97f00c31b5da3c
PETER ABBOTT <abbotsayshi@gmail.com>
2024-02-01 11:50:51 +0000

    save

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -6,9 +6,11 @@ import { UserButton } from "@clerk/nextjs";
 export default function Navigation() {
   return (
     <section className="NavigationWrapper">
-      <h1>Splatter</h1>
+      <h1 className="NavigationHeader">Splatter</h1>
       <NavigationMenu.Root>
-        <NavigationMenu.Link href="/mypaints">My paints</NavigationMenu.Link>
+        <NavigationMenu.Link href="/mypaints" className="NavigationLink">
+          My paints
+        </NavigationMenu.Link>
       </NavigationMenu.Root>
       <UserButton afterSignOutUrl="/" />
     </section>
